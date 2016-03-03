11d97a8942464b9dbe840af2f25857284b044ffc
Dark Neo <neogeoman@gmail.com>
2016-03-02 22:52:16 -0600

    Update avatarep.lang.php
    update lang vars to newest version.

--- inc/languages/espanol/avatarep.lang.php
+++ inc/languages/espanol/avatarep.lang.php
@@ -1,17 +1,18 @@
 <?php
 /**
 *  Idioma Español
-*  Avatar en Temas y Foros v 2.8.1
+*  Avatar en Temas y Foros v 2.8.5
 *
-*  Sitio Web: http://forosmybb.es
+*  Sitio Web: http://soportemybb.es
 *  Autor: Dark Neo - neogeoman@gmail.com
 */
-
-//Lista desplegable v2.0
 $l['avatarep_user_profile'] = "Ver Perfil";
 $l['avatarep_user_messages'] = "Ver Mensajes";
 $l['avatarep_user_sendpm'] = "Enviar MP";
 $l['avatarep_user_sendemail'] = "Enviar Correo";
 $l['avatarep_user_threads'] = "Ver Temas";
-
+$l['avatarep_user_error'] = " Error ";
+$l['avatarep_user_error_text'] = " Debes iniciar sesión para ver este contenido ";
+$l['avatarep_user_alt'] = "Avatar de {1}";
+$l['avatarep_user_no_avatar'] = "Sin avatar";
 ?>
