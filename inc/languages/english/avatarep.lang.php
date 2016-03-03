dee6c6c8ba6354d3f8d76f5108a71ab647b2c6fb
Dark Neo <neogeoman@gmail.com>
2016-03-02 22:51:08 -0600

    Update avatarep.lang.php
    updating info for new release.

--- inc/languages/english/avatarep.lang.php
+++ inc/languages/english/avatarep.lang.php
@@ -1,16 +1,18 @@
 <?php
 /**
 *  Language English
-*  Last Poster Avatar on Threads and Forums v 2.8.1
+*  Last Poster Avatar on Threads and Forums v 2.8.5
 *
 *  Sitio Web: http://forosmybb.es
 *  Autor: Dark Neo - neogeoman@gmail.com
 */
-
-//Desplegable List v2.0
 $l['avatarep_user_profile'] = "View Profile";
 $l['avatarep_user_messages'] = "View Messages";
 $l['avatarep_user_sendpm'] = "Send PM";
 $l['avatarep_user_sendemail'] = "Send E-mail";
 $l['avatarep_user_threads'] = "View Threads";
+$l['avatarep_user_error'] = " Error ";
+$l['avatarep_user_error_text'] = " You must login to see this content ";
+$l['avatarep_user_alt'] = "{1}'s Avatar";
+$l['avatarep_user_no_avatar'] = "No avatar";
 ?>
