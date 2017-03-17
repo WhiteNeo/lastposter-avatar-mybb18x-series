cee429fd9fa03220c5ee941e2408d81367fbc50e
Dark Neo <neogeoman@gmail.com>
2017-03-17 17:10:09 -0600

    Update to 2.9.2 version
    Updatig to next version...

--- inc/languages/english/avatarep.lang.php
+++ inc/languages/english/avatarep.lang.php
@@ -1,7 +1,7 @@
 <?php
 /**
 *  Language English
-*  Last Poster Avatar on Threads and Forums v 2.8.7
+*  Last Poster Avatar on Threads and Forums v 2.9.2
 *
 *  Sitio Web: http://forosmybb.es
 *  Autor: Dark Neo - neogeoman@gmail.com
@@ -17,4 +17,7 @@ $l['avatarep_user_alt'] = "{1}'s Avatar";
 $l['avatarep_user_no_avatar'] = "No avatar";
 $l['avatarep_retrieving'] = "Retrieving Data";
 $l['avatarep_loading'] = "Loading...";
-?>
+$l['avatarep_user_alt_forums'] = "Last poster: {1}";
+$l['avatarep_user_alt_thread_first'] = "Firts post by: {1}";
+$l['avatarep_user_alt_thread_last'] = "Last post by: {1}";
+$l['avatarep_user_alt_thread_contributor'] = "Thread Contributor: {1}";
