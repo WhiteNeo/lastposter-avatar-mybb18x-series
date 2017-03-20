83c38ed10ee75c5c55e700cc97b5d6e2c591022c
Dark Neo <neogeoman@gmail.com>
2017-03-20 17:07:36 -0600

    New scripts for modal extends.
    This is an addition to make more usefull this mod thanks to @Eldenroot.

---
+++ jscripts/avatarep.js
@@ -0,0 +1,53 @@
+$(document).on("ready", function(){
+	var NavaT = 0;						
+	var myTimer;
+	$("a#"+lpaname+lpamyid).on("click", function(e){
+		e.preventDefault();	
+		return false;
+	});
+	$("a#"+lpaname+lpamyid).on("mouseenter", function(){
+	var Nava = lpamyid;
+	var ID_href = $(this).attr("href");
+	var Data = "id=" + ID_href;
+	console.log(NavaT);
+	if(Nava != NavaT)
+	{
+		myTimer = setTimeout( function()
+		{			
+			$.ajax({
+				url:ID_href,
+				data:Data,
+				type:"post",
+				dataType:"json",
+				beforeSend:function()
+				{
+					$("div#"+lpaname+"mod"+lpamyid).css({
+						"display": "block",
+						"margin-top": "0px",
+						"margin-left": "0px",
+						"position": "absolute",
+						"width": 320														
+					});						
+					$("div#"+lpaname+"mod"+lpamyid).fadeIn("fast");										
+					$("div#"+lpaname+"mod"+lpamyid).html("<center><img src='images/spinner_big.gif' alt='Retrieving Data'><br>Loading...<br></center>");
+				},									
+				success:function(res){	
+					NavaT = lpamyid;
+					$("div#"+lpaname+"mod"+lpamyid+" div.modal_avatar").css("display","inline-block");
+					$("div#"+lpaname+"mod"+lpamyid).html(res);
+				}
+			});	
+		return false;
+		}, lpatimer);
+		}
+	else
+	{
+		$("div#"+lpaname+"mod"+lpamyid).fadeIn("slow");
+	}						
+	}).on("mouseleave", function(){
+		if(myTimer)
+		clearTimeout(myTimer);				
+		$("div#"+lpaname+"mod"+lpamyid).fadeOut("fast");
+		$(this).stop();
+	});
+});
